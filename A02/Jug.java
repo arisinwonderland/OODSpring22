140ee01718668339567f470b01061a431b70bfc1
Aris <arisyu03@gmail.com>
2022-02-12 03:14:39 +0000

    creating Jug.java

---
+++ A02/Jug.java
@@ -0,0 +1,14 @@
+public class Jug
+{
+   public static void main(String args[])
+   {
+      // you can use this to test your class
+      // but this will not be used by the grader...
+   }
+   
+   // state...
+
+   // constructors...
+
+   // behaviors...   
+}
