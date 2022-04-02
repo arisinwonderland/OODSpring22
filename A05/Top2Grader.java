d04e6ff22be2c881de425bf84b66b20946ca9f78
Aris Yu <arisyu03@gmail.com>
2022-04-02 02:19:07 +0000

    preliminary Top2Grader

---
+++ A05/Top2Grader.java
@@ -0,0 +1,37 @@
+class Top2Grader extends Grader
+{
+  private int m_curve;
+  
+  public Top2Grader(Integer ary[]) {
+    super(ary);
+  }
+  
+  @Override
+  public double calculateGrade() {
+    if (ary.length == 0)
+      return 0.0;
+    else if (ary.length == 1)
+      return ary[0];
+
+    double g1 = ary[0];
+    double g2 = ary[0];
+
+    for (int i = 1; i < ary.length; i++) {
+      if (ary[i] >= g2) {
+        if (ary[i] >= g1) {
+          g2 = g1;
+          g1 = ary[i];
+        } else {
+          g2 = ary[1];
+        }
+      }
+    }
+
+    return (g1 + g2)/2.0;
+  }
+  
+  @Override
+  public String toString() { 
+    return "Top2Grader"; 
+  }
+}
