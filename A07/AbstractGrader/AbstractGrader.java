957127c21e3f5563d767fcf3f158111805ff6f03
Aris Yu <arisyu03@gmail.com>
2022-04-09 01:43:11 +0000

    filled out AbstractGrader class

--- A07/AbstractGrader/AbstractGrader.java
+++ A07/AbstractGrader/AbstractGrader.java
@@ -1,4 +1,11 @@
 public abstract class AbstractGrader implements IGrader {
+  protected IFilter m_filter;
+  protected int[] m_grades;
+
+  public AbstractGrader(int[] grades, IFilter filter) {
+    m_grades = grades;
+    m_filter = filter;
+  }
   
   public abstract double grade();
 }
