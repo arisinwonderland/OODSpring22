a5ff26a3c25372f9052aed90b2efa83df1d6cac5
Aris Yu <arisyu03@gmail.com>
2022-04-30 03:35:55 +0000

    compiled and added Builder return statement

--- A08/Wizard/Builder.java
+++ A08/Wizard/Builder.java
@@ -39,5 +39,7 @@ public class Builder {
     Decision snowboardDecision = new Decision("Do you want to buy a snowboard?");
     snowboardDecision.setNo(skiDecision);
     snowboardDecision.setYes(snowboardingDecision);
+
+    return snowboardDecision;
   }
 }
