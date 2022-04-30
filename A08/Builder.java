0bac2adf3737154533cecfc1f0407029a27757de
Aris Yu <arisyu03@gmail.com>
2022-04-30 02:56:31 +0000

    began the mess that is Builder.java

--- A08/Builder.java
+++ A08/Builder.java
@@ -6,4 +6,9 @@ public class Builder {
     return m_instance;
   }
   private Builder() {}
+  public ITrip createTrip() {
+    ITrip trip = new ComplexTrip("North Adams");
+    ITrip temp = new ComplexTrip("MASSMoCA").add(new SimpleTrip("Lewitt Exhibit")).add(new SimpleTrip("Cafe"));
+    trip.add(temp).add(new SimpleTrip("MCLA"));
+  }
 }
